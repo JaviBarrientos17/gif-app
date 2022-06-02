3dfb8d7ee21c512a74c4a59f012c6fcbba2abdf1
Javi Barrientos <44582104+JaviBarrientos17@users.noreply.github.com>
2022-06-02 16:47:02 +0200

    Changing styles

--- src/components/GifExpertApp/GifExpertApp.js
+++ src/components/GifExpertApp/GifExpertApp.js
@@ -2,18 +2,10 @@ import React from "react";
 import { AddGif } from "../AddGif/AddGif";
 
 export const GifExpertApp = () => {
-  const [gifs, setGifs] = React.useState([
-    "One Punch",
-    "Samurai X",
-    "Dragon Ball",
-  ]);
-  // const handleAddGif = () => {
-  //   setGifs([...gifs, 'Prueba GIF']);
-  // }
+  const [gifs, setGifs] = React.useState(["One Punch"]);
 
   return (
     <div>
-      <h2>GifExpertApp</h2>
       <AddGif setGifs={setGifs} />
       <hr />
       <ol>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { GifExpertApp } from './components/GifExpertApp/GifExpertApp';
+import { Navbar } from './components/Navbar/Navbar';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <GifExpertApp />
+  <Navbar />
 );
 
 // If you want to start measuring performance in your app, pass a function
